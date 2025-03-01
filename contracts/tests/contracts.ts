d1fa36b2c2343c74c1023f7b8ea7cf3a8325ebce
ahmed <ahmxd989@gmail.com>
2025-03-02 00:23:34 +0400

    added test for smart contracts

--- contracts/tests/contracts.ts
+++ contracts/tests/contracts.ts
@@ -1,16 +1,38 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { BN, Program, Provider } from "@coral-xyz/anchor";
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js"
+import { ProgramTestContext, startAnchor } from "solana-bankrun"
+import { BankrunProvider } from "anchor-bankrun"
 import { Contracts } from "../target/types/contracts";
 
+const IDL = require('../target/idl/contracts.json');
+const contractAddress = new PublicKey("FNUwDxfH4cwnM4HSkNxDivN4o5n7U17fVMchqrtkTJBp");
+
 describe("contracts", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  let context: ProgramTestContext;
+  let provider: BankrunProvider;
+  let stakeProgram: Program<Contracts>;
+  const walletAddress = new PublicKey("DwNHVjdwU1ivHgSEmaNncvsmCmqM4EPB6mEqNGBayPVY")
 
-  const program = anchor.workspace.Contracts as Program<Contracts>;
+  before(async () => {
+    context = await startAnchor("", [{ name: "contracts", programId: contractAddress }], [])
+    provider = new BankrunProvider(context);
+    stakeProgram = new Program<Contracts>(IDL, provider);
+  })
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+  it("initialize user", async () => {
+    const txSignature = await stakeProgram.methods.initializeUserPda(
+      walletAddress,
+      new BN(1),
+      true,
+      false
+    ).rpc()
+
+    console.log("transaction Signature", txSignature)
+
+    const [user_pda_address] = PublicKey.findProgramAddressSync([Buffer.from("user"), walletAddress.toBuffer()] , stakeProgram.programId);
+    const user_pda = await stakeProgram.account.userPda.fetch(user_pda_address);
+
+    console.log(user_pda);
+  })
+
 });
