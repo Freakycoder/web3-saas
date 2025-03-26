d67664a16eee159e4a728d039c045fb74944cab2
ahmed <ahmxd989@gmail.com>
2025-03-26 04:05:31 +0400

    added jup wallet

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,28 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import {
-  WalletModalProvider,
-} from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css'; // Required for UI components
+import { UnifiedWalletProvider} from '@jup-ag/wallet-adapter';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const network = WalletAdapterNetwork.Devnet; // Change to 'mainnet-beta' for production
-  const endpoint = clusterApiUrl(network);
-
-  const wallets = [new UnsafeBurnerWalletAdapter()];
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets}>
-        <WalletModalProvider>
-
-          <Component {...pageProps} />
-
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider >
+      <UnifiedWalletProvider wallets={[]} config={{
+        theme: "dark",
+        autoConnect: false,
+        env: "devnet", // Change to 'mainnet-beta' for production
+        metadata: {
+          name: "Eyeballs",
+          description: "web3 powered platform",
+          url: 'https://jup.ag',
+          iconUrls : ['https://jup.ag/favicon.ico']
+        }
+      }}>
+        <Component {...pageProps} />
+      </UnifiedWalletProvider>
   );
 }
