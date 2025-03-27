c8c24fcfe5a905d42284b4565f6be9ef5e86838e
ahmed <ahmxd989@gmail.com>
2025-03-27 05:03:37 +0400

    updated app

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,22 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import '@solana/wallet-adapter-react-ui/styles.css'; // Required for UI components
-import { UnifiedWalletProvider} from '@jup-ag/wallet-adapter';
+import { UnifiedWalletProvider } from '@jup-ag/wallet-adapter';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <UnifiedWalletProvider wallets={[]} config={{
-        theme: "dark",
-        autoConnect: false,
-        env: "devnet", // Change to 'mainnet-beta' for production
-        metadata: {
-          name: "Eyeballs",
-          description: "web3 powered platform",
-          url: 'https://jup.ag',
-          iconUrls : ['https://jup.ag/favicon.ico']
-        }
-      }}>
-        <Component {...pageProps} />
-      </UnifiedWalletProvider>
+    <UnifiedWalletProvider wallets={[]} config={{
+      theme: "jupiter",
+      autoConnect: false,
+      env: "devnet", // Change to 'mainnet-beta' for production
+      metadata: {
+        name: "Eyeballs",
+        description: "web3 powered platform",
+        url: '',
+        iconUrls: []
+      }
+    }}>
+      <Component {...pageProps} />
+    </UnifiedWalletProvider>
   );
 }
