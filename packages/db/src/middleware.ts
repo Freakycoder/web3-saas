4909f56c3c31082edd0aa12340aeb1956b71bd9a
ahmed <ahmxd989@gmail.com>
2025-03-28 01:25:59 +0400

    updated middleware

--- packages/db/src/middleware.ts
+++ packages/db/src/middleware.ts
@@ -2,42 +2,23 @@ import jwt from "jsonwebtoken"
 import { NextFunction, Request, Response } from "express"
 export const secreatKey = 'SeCrEaT'
 
-export const userMiddleware = (req: Request, res: Response, next : NextFunction) => {
-    const authHeader = req.headers['authorization'] ?? '';
-    const data = jwt.verify(authHeader, secreatKey );
+export const userMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization']!;
+    const token = authHeader?.split(' ')[1];
+    const data = jwt.verify(token, secreatKey);
 
-    if(!data){
-        res.status(400).json({message : "no wallet address found"});
+    if (!data) {
+        res.status(400).json({ message: "no wallet address found" });
         return
     }
     // @ts-ignore
-    if(data.userID){
+    if (data.userID) {
         //@ts-ignore
         req.userID = data.userID;
         next();
     }
-    else{
-        res.status(403).json({message : "please connect wallet"})
-        return
-    }
-}
-
-export const workerMiddleware = (req: Request, res: Response, next : NextFunction) => {
-    const authHeader = req.headers['authorization'] ?? '';
-    const data = jwt.verify(authHeader, secreatKey );
-
-    if(!data){
-        res.status(400).json({message : "no wallet address found"});
-        return
-    }
-    // @ts-ignore
-    if(data.userID){
-        //@ts-ignore
-        req.userID = data.userID;
-        next();
-    }
-    else{
-        res.status(403).json({message : "please connect wallet"})
+    else {
+        res.status(403).json({ message: "please connect wallet" })
         return
     }
 }
